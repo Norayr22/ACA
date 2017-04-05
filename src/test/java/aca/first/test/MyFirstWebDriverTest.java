d4ffbe28effdf38e352022a6ff06ac34470e4fc6
Your Name <you@example.com>
2017-04-05 22:05:07 +0500

    ‘msg‘

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -3,16 +3,18 @@ package aca.first.test;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstWebDriverTest {
 	
 	@Test()
 	public void myFirstTest(){
-		 WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver_win32\\Chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
 		 driver.get("http://aca.am");
 		 driver.close();
 		 driver.quit();
 	}
 
 }
+git status...git commit..git push origin master
