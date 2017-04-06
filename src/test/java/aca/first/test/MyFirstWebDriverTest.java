be100fd510055d0f28ebd9365a4855403cdc1c6d
Your Name <you@example.com>
2017-04-06 23:20:59 +0500

    msg

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,20 +1,58 @@
 package aca.first.test;
 
-import org.testng.annotations.Test;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class MyFirstWebDriverTest {
-	
-	@Test()
-	public void myFirstTest(){
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver_win32\\Chromedriver.exe");
-		 WebDriver driver = new ChromeDriver();
-		 driver.get("http://aca.am");
-		 driver.close();
-		 driver.quit();
-	}
+    private final String WEB_DRIVE_TYPE = "webdriver.chrome.driver";
+    private final String WEB_DRIVER_EXE_PATH = "C:\\Users\\USER\\Downloads\\chromedriver_win32\\Chromedriver.exe";
+    private final String URL = "http://totobet.am";
+    private WebDriver webDriver;
+    private String xpath = "//a[@class='tl_register_button']";
+    private String xpath1 = "//a[@href='https://sport.totogaming.am/SportsBook/Home?l=ru&d=d']";
+    private String verificationMessage;
+
+    @Before
+    public void setUp() {
+        System.setProperty(WEB_DRIVE_TYPE, WEB_DRIVER_EXE_PATH);
+        webDriver = new ChromeDriver();
+        verificationMessage="";
+    }
+
+    @Test
+    public void myFirstTest() {
+        webDriver.get(URL);
+        try {
+            Assert.assertTrue(webDriver.findElements(By.xpath(xpath)).size() != 0);
+        }catch (Error e){
+            verificationMessage = e.getMessage();
+            Assert.fail(verificationMessage);
+        }
+        try {
+            Assert.assertTrue( webDriver.findElements(By.xpath(xpath1)).size() !=0);}
+            catch (Error e){
+                verificationMessage = e.getMessage();
+                Assert.fail(verificationMessage);
+            }
+        }
+
+    
+
+    @After
+    public void close(){
+        webDriver.close();
+		webDriver.quit();
+
+    }
+
 
 }
-git status...git commit..git push origin master
+
