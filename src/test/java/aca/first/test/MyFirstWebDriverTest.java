cced18238a515778b1abc2b7b2fcdd0663a628fd
Your Name <you@example.com>
2017-04-08 12:45:18 +0500

    case

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -1,15 +1,15 @@
 package aca.first.test;
 
-import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+
+
 
 public class MyFirstWebDriverTest {
     private final String WEB_DRIVE_TYPE = "webdriver.chrome.driver";
@@ -26,7 +26,22 @@ public class MyFirstWebDriverTest {
         webDriver = new ChromeDriver();
         verificationMessage="";
     }
-
+/* Description: Verify if it is possible to place a bet (pre-match)
+Precondition: Will be a registered user.
+1. Open any browser,
+2. Go to https://totogaming.am,
+3. Go to Sport,
+4. Press the Login button,
+5. In the opened window enter valid login and password,
+6. Press login,
+7. Choose any kind of sport, country, tournament, event,
+8. From the choosen event press on any coeficient,
+9. Enter the amount or the bet (>=50 AMD),
+10. Press "Place Bet",
+Expected result: The message "Bet placed. Good Luck!" will appear and the balance will be less of the amount of the bet and the bet history will be apdated by that bet.
+The same is acctual also for In-Play bets. Only 3 point will be "Go to In-Play".
+ * 
+ */
     @Test
     public void myFirstTest() {
         webDriver.get(URL);
@@ -35,7 +50,7 @@ public class MyFirstWebDriverTest {
         }catch (Error e){
             verificationMessage = e.getMessage();
             Assert.fail(verificationMessage);
-        }
+        }  
         try {
             Assert.assertTrue( webDriver.findElements(By.xpath(xpath1)).size() !=0);}
             catch (Error e){
